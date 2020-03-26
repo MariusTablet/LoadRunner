889bc5350f341180f695d8bfee2758491c9903d5
unknown <mariu@DESKTOP-5OMHBD2>
2020-03-26 11:04:38 +0200

    This is my commit from my WIndows 10 from my MacBook Pro

--- Action.c
+++ Action.c
@@ -4,6 +4,8 @@ Action()
 	//This is my first github commit.
 	lr_start_transaction("01_Demo");
 	
+	//I am running on Mac using Windows 10 because LoadRunner doesn't currently support any MacOSs :(.s
+	
 	lr_end_transaction("01_Demo", LR_AUTO);
 	//This is my first github commit.
 	
