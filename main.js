a33674d96421a11a17312d357543fb94f5da62a2
unknown <mariu@DESKTOP-5OMHBD2>
2020-03-28 18:57:33 +0200

    This is my first DevWeb script

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿load.initialize(async function () {
+});
+
+load.action("Action", async function () {
+	load.log("Hello Marius, welcome to DevWeb protocol");
+});
+
+load.finalize(async function () {
+});
